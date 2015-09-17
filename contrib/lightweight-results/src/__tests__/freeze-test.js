e88772ba38e8189c192d935fba4996b0b546d7b2
Greg Hurrell <glh@fb.com>
2015-09-16 17:08:48 -0700

    query() -> result()

--- contrib/lightweight-results/src/__tests__/freeze-test.js
+++ contrib/lightweight-results/src/__tests__/freeze-test.js
@@ -4,7 +4,7 @@ const freeze = require('../freeze');
 
 describe('freeze()', () => {
   it('passes the fixture file', () => {
-    const query = require('../../fixtures/result');
-    expect(freeze(query).hasOwnProperty('data')).toBe(true);
+    const result = require('../../fixtures/result');
+    expect(freeze(result).hasOwnProperty('data')).toBe(true);
   });
 });
